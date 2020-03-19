3931702024723fa42955789b8aa4a6d97e48df85
王鹏泰 <2358326343@qq.com>
2020-03-20 02:28:25 +0800

    add REAADME

--- src/config/Configuration.java
+++ src/config/Configuration.java
@@ -2,5 +2,7 @@ package config;
 
 public class Configuration {
     public static String QI_NIU = "";
-    public static String TEMP_FILE_PATH = "";
+    public static final String TEMP_FILE_PATH = "";
+    public static final String IMAGE_FORMAT_PNG = "png";
+    public static final String IMAGE_FORMAT_JPG = "jpg";
 }
